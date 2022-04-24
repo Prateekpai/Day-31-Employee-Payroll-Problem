f966b6a7e97d7a08bfff4d8e3ab8423ad41f8e4e
Prateekpai <prateekvasanthpai@gmail.com>
2022-04-24 10:03:58 +0530

    Create employee_payroll table

---
+++ UC2.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE payroll_service; 
+
+create table employee_payroll
+(
+id int identity,
+name varchar(250),
+salary int,
+startdate date,
+
+);
+
+select * from employee_payroll
+
+
+
