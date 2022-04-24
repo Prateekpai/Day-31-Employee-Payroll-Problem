17b1484bdf53243185a7af91f3eddb03074bf17d
Prateekpai <prateekvasanthpai@gmail.com>
2022-04-24 10:05:13 +0530

    Insert data into table

---
+++ UC3.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE payroll_service; 
+
+create table employee_payroll
+(
+id int identity,
+name varchar(250),
+salary int,
+startdate date,
+
+);
+
+select * from employee_payroll
+
+insert into employee_payroll values ('Prateek',20000, '2020-02-14');
+
+
+
